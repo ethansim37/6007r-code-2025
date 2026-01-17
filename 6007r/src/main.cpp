93a0dd82f5bdb5cc5108c39000eb9b7f2debef75
SirDoggo <jasperkant@gmail.com>
2026-01-16 16:41:13 -0800

    a

--- 6007r/src/main.cpp
+++ 6007r/src/main.cpp
@@ -1,4 +1,61 @@
 #include "main.h"
+#include "lemlib/chassis/chassis.hpp"
+#include "lemlib/chassis/chassis.hpp"
+#include "lemlib/chassis/trackingWheel.hpp"
+#include "pros/abstract_motor.hpp"
+#include "pros/adi.hpp"
+#include "pros/distance.hpp"
+#include "pros/imu.hpp"
+#include "pros/motor_group.hpp"
+#include "pros/motors.hpp"
+#include "pros/optical.hpp"
+#include "pros/rotation.hpp"
+#include <cstddef>
+
+
+
+	pros::Controller master(pros::E_CONTROLLER_MASTER);
+
+
+  pros::adi::Pneumatics hood(2, true);
+	pros::MotorGroup left_mg({-16, -3, -15});    // Creates a motor group with forwards ports 1 & 3 and reversed port 2
+	pros::MotorGroup right_mg({9, 18, 19});  // Creates a motor group with forwards port 5 and reversed ports 4 & 6
+
+  pros::Motor intake(-2);
+  pros::Motor upperIntake(13);
+
+  pros::IMU imu(7);  // Inertial sensor on port 7
+
+  lemlib::Drivetrain drivetrain(&left_mg, &right_mg, 11.5, lemlib::Omniwheel::NEW_325, 450, 8);
+
+  lemlib::ControllerSettings lateralSettings(6, 0, 7, 0.45, 1.3, 300, 0.8, 250,
+                                           18);
+
+
+lemlib::ControllerSettings
+    angularSettings(2.2,    // proportional gain (kP)
+                    0.0015, // integral gain (kI)
+                    14.5,   // derivative gain (kD)
+                    6.0,    // anti windup
+                    0.5,    // small error range, in inches
+                    150,    // small error range timeout, in milliseconds
+                    0,      // large error range, in inches
+                    0,      // large error range timeout, in milliseconds
+                    0       // maximum acceleration (slew)
+    );
+
+
+lemlib::OdomSensors sensors(NULL, // vertical tracking wheel 1, set to null
+                            nullptr, // vertical tracking wheel 2, set to nullptr as we are using IMEs
+                            NULL, // horizontal tracking wheel 1
+                            nullptr, // horizontal tracking wheel 2, set to nullptr as we don't have a second one
+                            &imu // inertial sensor
+);
+  lemlib::Chassis chassis(drivetrain, lateralSettings, angularSettings, sensors);
+
+
+
+
 
 /**
  * A callback function for LLEMU's center button.
@@ -27,6 +84,14 @@ void initialize() {
 	pros::lcd::set_text(1, "Hello PROS User!");
 
 	pros::lcd::register_btn1_cb(on_center_button);
+
+
+  
+
+
+
+
+
 }
 
 /**
@@ -74,21 +139,25 @@ void autonomous() {}
  * task, not resume it from where it left off.
  */
 void opcontrol() {
-	pros::Controller master(pros::E_CONTROLLER_MASTER);
-	pros::MotorGroup left_mg({1, -2, 3});    // Creates a motor group with forwards ports 1 & 3 and reversed port 2
-	pros::MotorGroup right_mg({-4, 5, -6});  // Creates a motor group with forwards port 5 and reversed ports 4 & 6
-
 
 	while (true) {
-		pros::lcd::print(0, "%d %d %d", (pros::lcd::read_buttons() & LCD_BTN_LEFT) >> 2,
-		                 (pros::lcd::read_buttons() & LCD_BTN_CENTER) >> 1,
-		                 (pros::lcd::read_buttons() & LCD_BTN_RIGHT) >> 0);  // Prints status of the emulated screen LCDs
+        chassis.tank(master.get_analog(pros::E_CONTROLLER_ANALOG_LEFT_Y), master.get_analog(pros::E_CONTROLLER_ANALOG_RIGHT_Y));
 
-		// Arcade control scheme
-		int dir = master.get_analog(ANALOG_LEFT_Y);    // Gets amount forward/backward from left joystick
-		int turn = master.get_analog(ANALOG_RIGHT_X);  // Gets the turn left/right from right joystick
-		left_mg.move(dir - turn);                      // Sets left motor voltage
-		right_mg.move(dir + turn);                     // Sets right motor voltage
-		pros::delay(20);                               // Run for 20 ms then update
+      if (master.get_digital(pros::E_CONTROLLER_DIGITAL_L2)) {
+      intake.move_voltage(12000);
+      upperIntake.move_voltage(5000);
+      hood.retract();
+
+    } else if (master.get_digital(pros::E_CONTROLLER_DIGITAL_R2)) {
+      intake.move_voltage(12000);
+      upperIntake.move_voltage(12000);
+      hood.extend();
+
+    } else {
+      intake.move_voltage(0);
+      upperIntake.move_voltage(0);
+            hood.retract();
+
+    }
 	}
 }
